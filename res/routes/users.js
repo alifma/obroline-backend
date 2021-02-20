b7012023853cd7e3c32b25305951f51d9eb5b76f
alifma <alifmaulana26@gmail.com>
2021-02-20 17:12:25 +0700

    Auth Guard Login

--- res/routes/users.js
+++ res/routes/users.js
@@ -1,12 +1,13 @@
 const { login, register, patchUser, detailUser } = require('../controllers/users')
 const { singleUpload } = require('../helpers/upload')
+const { authentication } = require('../helpers/auth')
 const express = require('express')
 
 const route = express.Router()
 route
   .post('/api/login', login)
   .post('/api/register', register)
-  .patch('/api/user/:id', singleUpload, patchUser)
-  .get('/api/user/:id', detailUser)
+  .patch('/api/user/:id', authentication, singleUpload, patchUser)
+  .get('/api/user/:id', authentication, detailUser)
 
 module.exports = route
